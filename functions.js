3c3d844a64cc3aa8cfcb31a852634c4b17b2a922
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-19 12:37:52 +0100

    JS2 Week2 Assig1 Answers

--- functions.js
+++ functions.js
@@ -1,10 +1,35 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (pArr) => {
+  let newArr =[]
+  pArr.map(number => {
+    if (number %2 != 0) {
+    newArr.push(number)
+  }})
+  return newArr.length
+};
 
-const getIndexOfFalseItem = () => {};
 
-const filterNumbers = () => {};
+const getIndexOfFalseItem = (pArr) => {
+  let getIndexOfFalse = pArr.indexOf(false) 
+  return getIndexOfFalse
+};
 
-const generateEmailAddress = () => {};
+const filterNumbers = (pArr) => {
+  let filteredArr = []
+  pArr.map((item) => {
+    if(typeof item == "number" ){
+      filteredArr.push(item)
+    }
+  })
+  return filteredArr
+};
+
+const generateEmailAddress = (pArr) => {
+  let newArr = []
+  pArr.map((item)=>{
+    newArr.push(item.toLowerCase()+"@gmail.com")
+  })
+  return newArr
+};
 
 export {
   findTheNumberOfOddNumbers,
@@ -12,4 +37,3 @@ export {
   filterNumbers,
   generateEmailAddress,
 };
-/* UPDATE */
